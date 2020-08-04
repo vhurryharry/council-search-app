e9056d1500fc21cd4864e54243c980a5f9375f38
Alexander Bai <vhurryharry@gmail.com>
2020-08-04 13:35:26 -0400

    Implement the JWT interceptor.

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -5,24 +5,36 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InterceptorService implements HttpInterceptor{
-  constructor() { }
+export class InterceptorService implements HttpInterceptor {
+  constructor(private loginService: LoginService) { }
 
-  handleError(error: HttpErrorResponse){
-    console.log("Could not request data from server: "+error?.message);
+  handleError(error: HttpErrorResponse) {
+    console.log('Could not request data from server: ' + error?.message);
     return throwError(error);
   }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler):
-    Observable<HttpEvent<any>>{
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    const user = this.loginService.getCurrentUser();
+    if (user) {
+      const token = user.access_token;
+      let newHeaders = req.headers;
+      if (token) {
+        newHeaders = newHeaders.append('Authorization', 'Bearer ' + token);
+      }
+
+      req = req.clone({ headers: newHeaders });
+    }
+
     return next.handle(req)
       .pipe(
         catchError(this.handleError)
-      )
-  };
+      );
+  }
 
 }
